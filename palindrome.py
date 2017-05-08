1d5f8b90269eb4c39f33a3e41bccff3de5d642a5
John Wojtas <jwojtas11@gmail.com>
2017-05-08 11:31:32 -0400

    comments

--- palindrome.py
+++ palindrome.py
@@ -1,6 +1,6 @@
 original = input ("Enter a string: ")
-chkr = original[::-1]
-if original == chkr:
+chkr = original[::-1] #reverses string
+if original == chkr: #checks if reversed string is equal to original
   print ('\n{0} is a palindrome'.format(original))
 else:
   print('\n Not a palindrome') 
